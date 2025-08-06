49149338a32ec6ddafcac61e2e6f222d82ae58bf
YogaDharma21 <yogacode2006@gmail.com>
2025-08-07 06:21:20 +0800

    feat: Implement forgot password functionality with OTP verification and email notifications

---
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\SendForgotPasswordOTP;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\ResponseFormatter;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class ForgotPasswordController extends Controller
+{
+    public function request()
+    {
+        $validator = Validator::make(request()->all(), [
+            'email' => 'required|email|exists:users,email',
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error(
+                400,
+                $validator->errors(),
+            );
+        }
+        $check = DB::table('password_reset_tokens')->where('email', request()->email)->count();
+
+        if ($check > 0) {
+            return ResponseFormatter::error(400, null, ['Anda sudah melakukan ini, silahkan resend OTP']);
+        }
+
+        do {
+            $otp = rand(100000, 999999);
+            $otpCount = DB::table('password_reset_tokens')->where('token', $otp)->count();
+        } while ($otpCount > 0);
+
+        DB::table('password_reset_tokens')->insert([
+            'email' => request()->email,
+            'token' => $otp,
+        ]);
+
+        $user = User::whereEmail(request()->email)->firstOrFail();
+
+        Mail::to($user->email)->send(new SendForgotPasswordOTP($user, $otp));
+
+        return ResponseFormatter::success(
+            [
+                'is_sent' => true
+            ]
+        );
+    }
+
+    public function resendOtp()
+    {
+        $validator = Validator::make(request()->all(), [
+            'email' => 'required|email|exists:users,email',
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error(
+                400,
+                $validator->errors(),
+            );
+        }
+
+        $otpRecord = DB::table('password_reset_tokens')->where('email', request()->email)->first();
+
+        if (is_null($otpRecord)) {
+            return ResponseFormatter::error(400, null, ['request tidak ditemukan']);
+        }
+
+        $user = User::whereEmail(request()->email)->firstOrFail();
+
+        do {
+            $otp = rand(100000, 999999);
+            $otpCount = DB::table('password_reset_tokens')->where('token', $otp)->count();
+        } while ($otpCount > 0);
+
+        DB::table('password_reset_tokens')->where('email', request()->email)->update([
+            'token' => $otp,
+        ]);
+
+        Mail::to($user->email)->send(new SendForgotPasswordOTP($user, $otp));
+
+        return ResponseFormatter::success(
+            [
+                'is_sent' => true
+            ]
+        );
+
+    }
+
+    public function verifyOtp()
+    {
+        $validator = Validator::make(request()->all(), [
+            'email' => 'required|email|exists:users,email',
+            'otp' => 'required|exists:password_reset_tokens,token',
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error(
+                400,
+                $validator->errors(),
+            );
+        }
+
+        $check = DB::table('password_reset_tokens')->where('token', request()->otp)->where('email', request()->email)->count();
+
+        if ($check > 0) {
+            return ResponseFormatter::success([
+                'is_correct' => true
+            ]);
+        }
+
+        return ResponseFormatter::error([
+            400,
+            'Invalid OTP'
+        ]);
+    }
+
+    public function resetPassword()
+    {
+        $validator = Validator::make(request()->all(), [
+            'email' => 'required|email|exists:users,email',
+            'otp' => 'required|exists:password_reset_tokens,token',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error(
+                400,
+                $validator->errors(),
+            );
+        }
+
+        $token = DB::table('password_reset_tokens')->where('token', request()->otp)->where('email', request()->email)->first();
+
+        if (!is_null($token)) {
+            $user = User::whereEmail(request()->email)->first();
+
+            $user->update([
+                'password' => bcrypt(request()->password)
+            ]);
+
+            DB::table('password_reset_tokens')->where('token', request()->otp)->where('email', request()->email)->delete();
+            $token = $user->createToken(config('app.name'))->plainTextToken;
+
+            return ResponseFormatter::success([
+                'token' => $token
+            ]);
+        }
+        return ResponseFormatter::error(
+            400,
+            'Invalid OTP'
+        );
+    }
+}
---
+++ resources/views/mails/reset-password-otp.blade.php
@@ -0,0 +1,5 @@
+Hi {{ $user->name }}<br>
+
+Anda akan melakukan reset password, masukan OTP berikut kepada aplikasi : {{ $otp }}<br>
+
+Abaikan jika anda tidak melakukan reset password
