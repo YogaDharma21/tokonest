407ec218420578e8540078b4a4f71682bc2f5c65
YogaDharma21 <yogacode2006@gmail.com>
2026-01-19 18:22:33 +0800

    feat: Footer section

---
+++ app/components/layouts/HeaderOrange.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="text-white flex flex-col h-32">
+        <div class="bg-primary py-2">
+            <u-container class="flex justify-end divide-x divide-gray-50/50">
+                <u-button variant="link" color="white" class="p-0 px-3"
+                    >Daftar</u-button
+                >
+                <u-button variant="link" color="white" class="p-0 px-3"
+                    >Login</u-button
+                >
+            </u-container>
+        </div>
+        <div class="bg-linear-to-b from-primary to-[#ff6633] flex-1">
+            <u-container
+                class="flex justify-between h-full items-center gap-12"
+            >
+                <BaseLogo />
+                <LayoutsSearchBar class="flex-1" />
+                <u-button variant="link"><icon-cart /></u-button>
+            </u-container>
+        </div>
+    </header>
+</template>
+
+<script setup></script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,32 +1,10 @@
 <template>
     <div class="flex flex-col min-h-screen">
-        <header class="text-white flex flex-col h-32">
-            <div class="bg-primary py-2">
-                <u-container
-                    class="flex justify-end divide-x divide-gray-50/50"
-                >
-                    <u-button variant="link" color="white" class="p-0 px-3"
-                        >Daftar</u-button
-                    >
-                    <u-button variant="link" color="white" class="p-0 px-3"
-                        >Login</u-button
-                    >
-                </u-container>
-            </div>
-            <div class="bg-linear-to-b from-primary to-[#ff6633] flex-1">
-                <u-container
-                    class="flex justify-between h-full items-center gap-12"
-                >
-                    <BaseLogo />
-                    <LayoutsSearchBar class="flex-1" />
-                    <u-button variant="link"><icon-cart /></u-button>
-                </u-container>
-            </div>
-        </header>
+        <LayoutsHeaderOrange />
         <main class="flex-1 bg-blue-500">
             <slot />
         </main>
-        <footer>Footer</footer>
+        <LayoutsFooter />
     </div>
 </template>
 
